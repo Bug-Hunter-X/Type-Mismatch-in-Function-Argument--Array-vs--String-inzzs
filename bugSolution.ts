9797d0a386f8fd1dfa9320577b8a92e7161ebea6
BugHunter-X <askzenai@gmail.com>
2025-01-29 18:56:05 +0100

    Add bugSolution.ts file

---
+++ bugSolution.ts
@@ -0,0 +1,14 @@
+function greeter(person: string): string {
+  return "Hello, " + person;
+}
+
+let user = ["Jane User", "John Smith"];
+
+// Solution 1: Iterate through the array
+user.forEach(name => console.log(greeter(name)));
+
+// Solution 2: Change function signature to accept an array
+function greeterArray(people: string[]): string[] {
+  return people.map(person => "Hello, " + person);
+}
+console.log(greeterArray(user));
